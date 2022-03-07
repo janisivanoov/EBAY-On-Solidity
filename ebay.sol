aeef753f685f5ec61fdd7a82c80e99a64bc37932
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2022-03-07 11:53:08 +0200

    Add files via upload

---
+++ ebay.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.8.4;
+
+contract Auction {
+    
+    address payable public beneficiary;
+    uint public auctionEndTime;
+    
+    // current state of the auction 
+    address public highestBidder;
+    uint public highestbid; 
+    bool ended;
+    
+    mapping(address => uint) pendingReturns;
+    
+    event highestBidIncreased(address bidder, uint amount);
+    event auctionEnded(address winner, uint amount);
+    
+    constructor(uint _biddingTime, address payable _beneficiary) {
+        beneficiary = _beneficiary;
+        auctionEndTime = block.timestamp + _biddingTime; 
+}
+
+    function bid() public payable {
+        
+        if(block.timestamp > auctionEndTime) revert('the auction has ended!');
+        
+        if(msg.value <= highestbid) revert('sorry, the bid is not high enough!');
+        
+        if(highestbid != 0) {
+            pendingReturns[highestBidder] += highestbid;
+        }
+        
+        highestBidder = msg.sender;
+        highestbid = msg.value;
+        emit highestBidIncreased(msg.sender, msg.value);
+    }
+    
+    function withdraw() public payable returns(bool) {
+        uint amount = pendingReturns[msg.sender];
+        if(amount > 0) {
+            pendingReturns[msg.sender] = 0;
+        }
+        
+        if(!payable(msg.sender).send(amount)) {
+            pendingReturns[msg.sender] = amount;
+        }
+        return true;
+    }
+    
+    function auctionEnd() public {
+        
+        if(block.timestamp < auctionEndTime) revert('the auction has not ended yet!');
+        if(ended) revert('the auction is already over!');
+        
+         ended = true;
+         emit auctionEnded(highestBidder, highestbid);
+         beneficiary.transfer(highestbid);
+    }
+
+}
